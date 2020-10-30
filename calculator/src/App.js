c2bdf8e5fd3bd50a79b4d447e89ad90b8a5040b7
omur8819 <67431733+omur8819@users.noreply.github.com>
2020-10-30 11:38:58 +0300

    project-update

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
+import React,{useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
+  const [currentSum,setCurrentSum]=useState(0);
+  const [clear,setClear]=useState(false);
+
+  useEffect(()=>{
+    document.querySelector('#result').value="";
+  },[])
+  
+  useEffect(()=>{
+    if(clear)
+    document.querySelector('#result').value="";
+  })
+
+  const Add=(e)=>{
+    e.preventDefault();
+    if(clear) setClear(false);
+    let currentNum=document.querySelector('#num').value
+    if(currentNum=='')
+    return;
+    let sum= currentSum+parseInt(currentNum);
+    setCurrentSum(sum);
+    document.querySelector('#num').value="";
+      
+  }
+
+  const Clear=(e)=>{
+    e.preventDefault();
+    console.log('sum:', currentSum);
+    document.querySelector('form').reset();
+    setClear(true);
+    setCurrentSum(0);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="app-title">
+        <h1> Basic Form Calculator</h1>
+      </div>
+      <form>
+            <input type="text" id="result" value={currentSum}  readOnly />   
+            <input type="text" id="num" placeholder="enter a number" />
+            <button onClick={Add}>Add</button>
+            <button onClick={Clear}>Clear</button>
+      </form>
     </div>
   );
 }
